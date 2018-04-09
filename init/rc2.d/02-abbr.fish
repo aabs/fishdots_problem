2dd3057bfc670545797a5998ff451cd8292e5049
Andrew Matthews <aabs@users.noreply.github.com>
2018-04-10 09:45:48 +1000

    common abbrs i've been creating

---
+++ init/rc2.d/02-abbr.fish
@@ -0,0 +1,6 @@
+abbr --add prb problem
+abbr --add prbs 'problem summarise'
+abbr --add prbk 'problem known'
+abbr --add prbq 'problem question'
+abbr --add prbi 'problem idea'
+abbr --add prbt 'problem test'
